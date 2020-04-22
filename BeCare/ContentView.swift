d85f4377f34cc8dfa1219ac0a29fcb8d2729ce5a
Giovanni Prisco <gprisco01@gmail.com>
2020-04-22 19:59:31 +0200

    Initial Commit

---
+++ BeCare/ContentView.swift
@@ -0,0 +1,42 @@
+//
+//  ContentView.swift
+//  BeCare
+//
+//  Created by Giovanni Prisco on 22/04/2020.
+//  Copyright © 2020 Giovanni Prisco. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var selection = 0
+ 
+    var body: some View {
+        TabView(selection: $selection){
+            Text("First View")
+                .font(.title)
+                .tabItem {
+                    VStack {
+                        Image("first")
+                        Text("First")
+                    }
+                }
+                .tag(0)
+            Text("Second View")
+                .font(.title)
+                .tabItem {
+                    VStack {
+                        Image("second")
+                        Text("Second")
+                    }
+                }
+                .tag(1)
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
